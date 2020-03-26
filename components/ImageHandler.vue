91e16de172aa3da995306263d5da7964bc84a250
couldbefree <zoriaalexandr@gmail.com>
2020-03-26 14:49:40 +0200

    Refactored image component

---
+++ components/ImageHandler.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+      <v-icon
+        v-if="index > -1"
+        color="grey lighten-1"
+        x-small
+        @click="onImageRemove"
+      >
+        mdi-close
+      </v-icon>
+    <v-img
+      max-height="300px"
+      :contain="true"
+      :src="getImageUrl()"
+    ></v-img>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ImageUpload",
+    props: ['image', 'index'],
+    data: () => ({
+      src: '',
+    }),
+    methods: {
+      onImageRemove () {
+        this.$emit('remove', this.index)
+      },
+      getImageUrl () {
+        if(this.image) {
+          return this.src = window.URL.createObjectURL(this.image);
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- components/ProductForm.vue
+++ components/ProductForm.vue
@@ -1,6 +1,10 @@
 <template>
   <v-row justify="center">
-    <v-dialog :value="value" max-width="600px">
+    <v-dialog
+      :value="value"
+      max-width="600px"
+      @click:outside="$emit('input', false)"
+    >
       <v-card color="accent">
         <v-card-title class="pb-0">
           <slot name="title"></slot>
@@ -35,50 +39,36 @@
               <v-col cols="12" sm="12" class="ma-0 pr-1 pl-0 pt-0 pb-0">
                 <v-file-input
                   accept="image/*"
-                  label="Головна картинка"
-                  @change="onInput('single', $event)"
+                  single
+                  label="Картинки товарів"
+                  v-model="image"
                 ></v-file-input>
-                <v-img
-                  max-height="200px"
-                  :contain="true"
-                  v-if="localImage"
-                  :src="localImage"
-                ></v-img>
+                <image-handler
+                  v-if="image"
+                  :image="image"
+                  :single="true"
+                ></image-handler>
               </v-col>
               <v-col cols="12" sm="12" class="ma-0 pl-1 pr-0 pt-0 pb-0">
                 <v-file-input
-                  @change="onInput('multiple', $event)"
                   accept="image/*"
-                  multiple label="Картинки товарів"
-                  v-model="imageItems"
+                  multiple
+                  label="Картинки товарів"
+                  v-model="images"
                 ></v-file-input>
               </v-col>
               <v-col
-                v-for="(image, index) in localImages"
-                cols="12"
-                sm="2"
-                class="pa-1"
-                style="text-align: right;"
+                v-if="images.length > 0"
+                cols="3"
+                xs="3"
+                v-for="(image, index) in images"
               >
-                <v-badge
-                  color="transparent"
-                  left
-                  style="cursor: pointer;"
-                >
-                  <v-icon
-                    color="grey lighten-1"
-                    x-small
-                    @click="onImageRemove(index)"
-                  >
-                    mdi-close
-                  </v-icon>
-                </v-badge>
-                <v-img
-                  max-height="300px"
-                  :contain="true"
-                  v-if="localImages.length > 0"
-                  :src="image"
-                ></v-img>
+                <image-handler
+                  :image="image"
+                  :index="index"
+                  :multiple="true"
+                  @remove="onImageRemove"
+                ></image-handler>
               </v-col>
             </v-row>
           </v-container>
@@ -95,42 +85,26 @@
 </template>
 
 <script>
+  import ImageHandler from "./ImageHandler";
+
   export default {
     name: "ProductForm",
     props: ['value'],
+    components: {
+      ImageHandler
+    },
     data: () => ({
       dialog: false,
       category: ['woman', 'man', 'baby-soap', 'bouquets', 'kits', 'natural'],
-      localImage: '',
-      localImages: [],
-      imageItems: [],
-      imageItem: '',
-      hover: false
+      image: null,
+      images: []
     }),
     methods: {
-      onInput(type, val) {
-        if(type === 'single' && val) {
-          this.localImage = window.URL.createObjectURL(val);
-          this.imageItem = val;
-        } else if(type === 'single' && !val) {
-          this.localImage = '';
-          this.imageItem = '';
-        }
-
-        if(type === 'multiple' && val && val.length) {
-          val.forEach((file) => {
-            this.localImages.push(window.URL.createObjectURL(file));
-          });
-        } else if(type === 'multiple' && val && !val.length) {
-          this.localImages = [];
-        }
-      },
-      onSave(){
+      onSave() {
         this.$emit('save');
       },
       onImageRemove(index) {
-        this.localImages.splice(index, 1);
-        this.imageItems.splice(index, 1);
+        this.images.splice(index, 1);
       }
     }
   }
