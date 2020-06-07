13fca9640de429a3f821e639b98d78c9c88a4cd4
couldbefree <zoriaalexandr@gmail.com>
2020-06-07 15:58:36 +0300

    images layout

--- pages/product/_id.vue
+++ pages/product/_id.vue
@@ -27,6 +27,20 @@
           <span class="label">Опис товару</span>
           <ckeditor :editor="editor" @input="onEditorUpdate"></ckeditor>
         </v-card>
+        <v-card
+          class="pa-6 mt-6"
+          outlined
+          light
+        >
+          <v-card-title class="pa-0">Картинки товару</v-card-title>
+          <div class="grid-container">
+            <div class="item" v-for="i in 19" :key="i"></div>
+            <div class="upload-button">
+              <input type="file" id="file" accept="image/*">
+              <label class="upload-label" for="file">Завантажити картинку</label>
+            </div>
+          </div>
+        </v-card>
       </v-col>
       <v-col lg="3"/>
     </v-row>
@@ -79,5 +93,48 @@
   .label {
     font-size: 13px;
     color: rgba(0, 0, 0, 0.6);
+  }
+
+  .grid-container {
+    display: grid;
+    grid-template-columns: repeat(5,1fr);
+    grid-gap: .8rem;
+  }
+
+  .item {
+    border: 1px solid red;
+    border-radius: 3px;
+  }
+
+  .item:first-child {
+    grid-column: 1/span 2;
+    grid-row: 1/span 2;
+    height: 100%;
+  }
+
+  .item:not(:first-child) {
+    height: 110px;
+  }
+
+  .upload-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    border-radius: 3px;
+    border: .2rem dashed #dfe3e8;
+    overflow: hidden;
+  }
+
+  input[type="file"] {
+    display: none;
+  }
+
+  .upload-label {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
   }
 </style>
